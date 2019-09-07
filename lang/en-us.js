aea56956b6adbabe4f9cea90e1dc8a9e1c1b31e9
Adeel Hussain <mr.adi180@gmail.com>
2019-09-07 22:40:01 +0500

    Added language folder

---
+++ lang/en-us.js
@@ -0,0 +1,17 @@
+$.extend(true, $.summernote.lang, {
+	'en-US': { /* US English(Default Language) */
+		imageAttributes: {
+			edit: 'Edit',
+			titleLabel: 'Title',
+			altLabel: 'Alternative Text',
+			captionLabel: 'Caption',
+			tooltip: 'Edit Image',
+			dialogSaveBtnMessage: 'Save',
+			dialogTitle: 'Change Image Attributes',
+			widthLabel: 'Width',
+			heightLabel: 'Height',
+			imageLockLabel: 'Lock',
+			resizeLabel: 'Reset the size to orignal'
+		}
+	}
+});
